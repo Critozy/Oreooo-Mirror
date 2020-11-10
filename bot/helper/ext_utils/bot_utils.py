addaca9aea9a5699c4d9cdf80ec0e62cb2376f63
Luqman_Reburn <46706073+LuqmanReburn@users.noreply.github.com>
2020-11-10 10:32:20 +0800

    last update

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -23,7 +23,7 @@ class MirrorStatus:
 
 
 PROGRESS_MAX_SIZE = 100 // 8
-PROGRESS_INCOMPLETE = ['●', '●', '●', '●', '●', '●', '●']
+PROGRESS_INCOMPLETE = ['🟡', '🔵', '🟡', '🔵', '🟡', '🔵', '🟡']
 
 SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
 

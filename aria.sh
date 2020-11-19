878be214a031e6c8bef3573125853ded2b475767
Luqman_Reburn <46706073+LuqmanReburn@users.noreply.github.com>
2020-11-19 14:05:12 +0800

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -1,6 +1,6 @@
 file="trackers.txt"
-echo "$(curl -Ns https://trackerslist.com/all_aria2.txt | awk '$1' | tr ',' '\n')" > trackers.txt
-echo "$(curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt)" >> trackers.txt
+echo "$(curl -Ns http://trackerslist.com/all_aria2.txt | awk '$1' | tr ',' '\n')" > trackers.txt
+echo "$(curl -Ns http://raw.githubusercontent.com/XIU2/TrackersListCollection/master/best.txt)" >> trackers.txt
 tmp=$(sort trackers.txt | uniq) && echo "$tmp" > trackers.txt
 sed -i '/^$/d' trackers.txt
 sed -z -i 's/\n/,/g' trackers.txt
@@ -11,7 +11,7 @@ fi
 tracker="[$tracker_list]"
 export MAX_DOWNLOAD_SPEED=0
 export MAX_CONCURRENT_DOWNLOADS=10
-aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 --check-certificate=false\
+aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 8210 --check-certificate=false\
    --max-connection-per-server=10 --rpc-max-request-size=1024M \
    --bt-tracker=$tracker --bt-max-peers=0 --bt-tracker-connect-timeout=300 --bt-stop-timeout=300 --seed-time=0.01 --min-split-size=10M \
    --min-split-size=10M --follow-torrent=mem --split=10 \

46157f1fd54d29c7e50b862c03855d1194914802
Luqman_Reburn <46706073+LuqmanReburn@users.noreply.github.com>
2020-12-06 09:08:36 +0800

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -1,6 +1,6 @@
 export MAX_DOWNLOAD_SPEED=0
 tracker_list=$(curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/best.txt | awk '$1' | tr '\n' ',')
-export MAX_CONCURRENT_DOWNLOADS=15
+export MAX_CONCURRENT_DOWNLOADS=10
 aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 \
   --max-connection-per-server=10 --rpc-max-request-size=1024M \
   --bt-tracker="[$tracker_list]" --bt-max-peers=0 --bt-stop-timeout=1800 --seed-time=0.01 \

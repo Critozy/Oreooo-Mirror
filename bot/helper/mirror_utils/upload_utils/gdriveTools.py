3c297803d5a1458f9d7ee0420c3d48e23db72d13
Luqman_Reburn <46706073+LuqmanReburn@users.noreply.github.com>
2020-12-24 05:02:45 +0800

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -482,9 +482,9 @@ class GoogleDriveHelper:
                     content += f'<b> | <a href="https://telegra.ph/{self.path[nxt_page]}">Next</a></b>'
                     nxt_page += 1
             Telegraph(access_token=telegraph_token).edit_page(path = self.path[prev_page],
-                                 title = 'Mirror Bot Search',
-                                 author_name='Mirror Bot',
-                                 author_url='https://github.com/magneto261290/magneto-python-aria',
+                                 title = 'Misaka Bot Search',
+                                 author_name='Misaka Bot',
+                                 author_url='https://github.com/LuqmanReburn/VPS-Mirror-Arata',
                                  html_content=content)
         return
 
